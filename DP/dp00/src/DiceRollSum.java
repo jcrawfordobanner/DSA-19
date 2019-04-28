b9add1c0a04417342e5e253765aad4705c5d091a
jcrawfordobanner <jordan.crawford-obanner@students.olin.edu>
2019-04-28 15:04:28 -0400

    sad

--- DP/dp00/src/DiceRollSum.java
+++ DP/dp00/src/DiceRollSum.java
@@ -1,10 +1,31 @@
+import java.util.HashMap;
+
 public class DiceRollSum {
 
     // Runtime: TODO
     // Space: TODO
     public static int diceRollSum(int N) {
         // TODO
-        return 0;
+        HashMap<Integer,Integer> answers = new HashMap<>();
+        return recurse(N,answers);
+    }
+
+    private static int recurse(int N, HashMap<Integer,Integer> memo){
+        int cost=0;
+        if(N==0){
+            return 1;
+        }
+        if(N<0){
+            return 0;
+        }
+        if(memo.containsKey(N)){
+            return memo.get(N);
+        }
+        for(int i = 1; i<7;i++){
+            cost+=recurse(N-i,memo);
+        }
+        memo.put(N,cost);
+        return cost;
     }
 
 }
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,44 @@
-public class LongestIncreasingSubsequence {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
+public class LongestIncreasingSubsequence {
+    public static int max =1;
     // Runtime: TODO
     // Space: TODO
     public static int LIS(int[] A) {
         // TODO
-        return 0;
+        max=1;
+        if(A.length==0){
+            return 0;
+        }
+        PriorityQueue<Integer> answers = new PriorityQueue<>();
+        int a =0;
+        int p= recurse(A,answers);
+        System.out.println(max);
+        System.out.println(p);
+        return max;
+    }
+
+    private static int recurse(int[] A, PriorityQueue<Integer> memo){
+        int len = 1;
+        if(A.length==1){
+            return 1;
+        }
+        for(int j=1;j<A.length;j++){
+            int k= recurse(Arrays.copyOfRange(A, 0, j), memo);
+            if(A[j-1]<A[A.length-1] && k+1>len){
+                len=k+1;
+                memo.add(len);
+            }
+            for(int ak: A){
+                System.out.print(ak + " ");
+            }
+            System.out.println("len "+len);
+        }
+        if(max<len){
+            max=len;
+        }
+        return len;
     }
 }
